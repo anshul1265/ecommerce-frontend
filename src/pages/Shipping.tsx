5a9b6ab38bef3ca48b9be110a7c9cfee0ddb95ff
Anshul <Anshul@anshul.com>
2024-04-06 11:22:12 +0530

    fixes in shipping pincode

--- src/pages/Shipping.tsx
+++ src/pages/Shipping.tsx
@@ -58,13 +58,13 @@ const Shipping = () => {
         <input required type="text" placeholder="Address" name="address" value={shippingInfo.address} onChange={changeHandler} />
         <input required type="text" placeholder="City" name="city" value={shippingInfo.city} onChange={changeHandler} />
         <input required type="text" placeholder="State" name="state" value={shippingInfo.state} onChange={changeHandler} />
+        <input required type="text" placeholder="Pincode" name="pinCode" value={shippingInfo.pinCode} onChange={changeHandler} />
         <select name="country" required value={shippingInfo.country} onChange={changeHandler}>
           <option value={""}>Choose Country</option>
           <option value={"India"}>India</option>
           <option value={"USA"}>USA</option>
           <option value={"UK"}>UK</option>
         </select>
-        <input required type="text" placeholder="Pincode" name="pincode" value={shippingInfo.pinCode} onChange={changeHandler} />
         <button type="submit">Pay Now</button>
       </form>
 
